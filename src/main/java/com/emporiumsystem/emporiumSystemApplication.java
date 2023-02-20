b56350d70711bdfe9b35e6b6d75ee58f2995febc
剑漫羽游 <1750365869@qq.com>
2023-02-20 16:38:03 +0000

    update some details.
    Signed-off-by: 剑漫羽游 <1750365869@qq.com>

--- src/main/java/com/emporiumsystem/emporiumSystemApplication.java
+++ src/main/java/com/emporiumsystem/emporiumSystemApplication.java
@@ -14,6 +14,12 @@ public class emporiumSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(emporiumSystemApplication.class, args);
+        System.out.println("   ___ _       _             __    __ _ _   _     __                       _\n" +
+                "  / __\\ |_   _(_)_ __   __ _/ / /\\ \\ (_) |_| |__ / _\\_      _____  _ __ __| |___\n" +
+                " / _\\ | | | | | | '_ \\ / _` \\ \\/  \\/ / | __| '_ \\\\ \\\\ \\ /\\ / / _ \\| '__/ _` / __|\n" +
+                "/ /   | | |_| | | | | | (_| |\\  /\\  /| | |_| | | |\\ \\\\ V  V / (_) | | | (_| \\__ \\\n" +
+                "\\/    |_|\\__, |_|_| |_|\\__, | \\/  \\/ |_|\\__|_| |_\\__/ \\_/\\_/ \\___/|_|  \\__,_|___/\n" +
+                "         |___/         |___/");
     }
 
 }
